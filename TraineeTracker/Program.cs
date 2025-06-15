5590751f64f9409d876c30ebff14e8474020a643
Komodorpudel <sah_services@outlook.com>
2025-06-15 11:34:06 +0200

    GmailEmailSender registered in Program.cs

--- TraineeTracker/Program.cs
+++ TraineeTracker/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using TraineeTracker.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,6 +28,18 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+
+
+// ----------------------------------------
+// Register Email Service
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
+builder.Services.AddTransient<IEmailSender, GmailEmailSender>();
+
+// ----------------------------------------
+
+
 
 var app = builder.Build();
 
