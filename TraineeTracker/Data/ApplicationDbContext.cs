a1119da0c1a9fc563978064999117846156a20b5
Komodorpudel <sah_services@outlook.com>
2025-06-17 12:05:13 +0200

    Sichergestellt, dass TraineeLessonState Enum als String und nicht Int in DB gespeichert wird (aber auskommentiert)

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -3,10 +3,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TraineeTracker.Data;
 
-public class ApplicationDbContext : IdentityDbContext
-{
+public class ApplicationDbContext : IdentityDbContext {
+
+
+    // Lists
+    /*
+    public DbSet<TraineeLesson> TraineeLessons { get; set; }
+    */
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
+        : base(options) {
     }
+
+    /*
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Enum to String Mapping
+        modelBuilder.Entity<TraineeLesson>()
+            .Property(t => t.State)
+            .HasConversion<string>();
+    }
+    */
+
 }
