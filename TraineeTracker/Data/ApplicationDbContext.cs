9186c2744376f7fbd20b1f41378f19ed7adcf8b3
highestPACE <pschweizer0711@gmail.com>
2025-06-18 19:51:46 +0200

    Added ApplicationUser and configured the DbContext and Identity to use ApplicationUser as the Userclass

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
---
+++ TraineeTracker/Models/Domain/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace TraineeTracker.Models.Domain {
+    public class ApplicationUser : IdentityUser {
+        
+    }
+}
