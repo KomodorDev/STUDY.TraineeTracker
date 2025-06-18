1577461da7382acb5607a256d2b3099ca42a324c
highestPACE <pschweizer0711@gmail.com>
2025-06-18 19:51:46 +0200

    Added ApplicationUser and configured the DbContext and Identity to use ApplicationUser as the Userclass

--- TraineeTracker/Program.cs
+++ TraineeTracker/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TraineeTracker.Data;
+using TraineeTracker.Models.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,14 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 var environment = builder.Environment;  // NEU: Environment auslesen
 Console.WriteLine($"🌍 Environment: {environment.EnvironmentName}");
 
-if (environment.IsDevelopment())
-{
+if (environment.IsDevelopment()) {
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseInMemoryDatabase("TestDb"));  // InMemory für Tests
-}
-else
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+} else {
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
         ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -24,19 +22,16 @@ else
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseMigrationsEndPoint();
-}
-else
-{
+} else {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
