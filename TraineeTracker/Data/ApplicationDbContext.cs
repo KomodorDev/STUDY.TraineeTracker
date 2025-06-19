9802eeb0c545314ef40eedae9d003e5889dc2e5e
stefanni <nikita.stefan@uni-a.de>
2025-06-19 14:25:03 +0200

    using namespace hinzugefügt

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Data;
 
@@ -7,7 +8,7 @@ public class ApplicationDbContext : IdentityDbContext
 {
 
     public DbSet<TraineeStatisticsSnapshot> TraineeStatisticsSnapshots { get; set; }
-    
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
