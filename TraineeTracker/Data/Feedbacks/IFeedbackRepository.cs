fac32cc4f6ee2472770e8751f5892e9cfb22c11c
KomodorDev <sah_services@outlook.com>
2025-06-19 22:32:56 +0200

    ReadByUser methode zu interface hinzugefügt

--- TraineeTracker/Data/Feedbacks/IFeedbackRepository.cs
+++ TraineeTracker/Data/Feedbacks/IFeedbackRepository.cs
@@ -9,6 +9,7 @@ namespace TraineeTracker.Data.Feedbacks {
 
         IEnumerable<Feedback> GetAllFeedbacksForLesson(Lesson lesson);
         IEnumerable<Feedback> GetAllFeedbacksWrittenByUser(ApplicationUser user);
+        IEnumerable<Feedback> GetAllFeedbacksReadByUser(ApplicationUser user);
         IEnumerable<Feedback> GetAllFeedbacksUnreadByUser(ApplicationUser user);
     }
 }
