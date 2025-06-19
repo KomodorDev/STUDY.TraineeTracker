dd4383d1f3f4547a5e6bf0956e6863e96e38ce96
stefanni <nikita.stefan@uni-a.de>
2025-06-19 14:13:12 +0200

    ApplicationDbContext um DbSet für TraineeStatisticsSnapshot ergänzt

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -5,6 +5,9 @@ namespace TraineeTracker.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+
+    public DbSet<TraineeStatisticsSnapshot> TraineeStatisticsSnapshots { get; set; }
+    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
