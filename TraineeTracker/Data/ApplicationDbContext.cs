4e3e206f039341b8e5749cf7ccc030bd7a994ee8
KomodorDev <sah_services@outlook.com>
2025-06-19 14:33:03 +0200

    EmailNotificationSettings Set zu ApplicationDbContext hinzugefügt

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Data;
 
-public class ApplicationDbContext : IdentityDbContext
-{
+public class ApplicationDbContext : IdentityDbContext {
+
+    public DbSet<EmailNotificationSettings> EmailNotificationSettings {
+        get; set;
+    }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
+        : base(options) {
     }
 }
