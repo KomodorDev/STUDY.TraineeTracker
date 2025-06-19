fc564bf54c4309b7314220bef593b2efae92400b
KomodorDev <sah_services@outlook.com>
2025-06-19 15:07:04 +0200

    Feedback DbSet zu Context hinzugefügt

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
-namespace TraineeTracker.Data;
+using TraineeTracker.Models.Domain;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    
+    public DbSet<Feedback> Feedbacks { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
+        : base(options) {
     }
 }
