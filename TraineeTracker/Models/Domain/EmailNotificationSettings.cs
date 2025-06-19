02753e50b487e714367e729b4a87d6be2e2380f6
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-06-19 14:32:45 +0200

    EmailNotificationSettings Model erstellt

---
+++ TraineeTracker/Models/Domain/EmailNotificationSettings.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TraineeTracker.Models.Domain
+{
+    public class EmailNotificationSettings
+    {
+        [Key]
+        public int EmailNotificationSettingsId { get; set; }
+
+        public bool ReceiveOpenNotifications { get; set; }
+        public bool ReceiveStartedNotifications { get; set; }
+        public bool ReceiveFinishedNotifications { get; set; }
+        public bool ReceiveRejectedNotifications { get; set; }
+        public bool ReceiveAcceptedNotifications { get; set; }
+        public bool ReceiveRatedNotifications { get; set; }
+
+        // Foreign key
+        public string UserId { get; set; } = null!;
+
+        // Navigation property
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
