81c70c8afa01d731ddc2c09de3d254ea65515eff
KomodorDev <sah_services@outlook.com>
2025-06-19 15:07:31 +0200

    IFeedbackRepository erstellt

---
+++ TraineeTracker/Data/Feedbacks/IFeedbackRepository.cs
@@ -0,0 +1,14 @@
+using TraineeTracker.Models.Domain;
+
+namespace TraineeTracker.Data.Feedbacks {
+    public interface IFeedbackRepository {
+        bool Exists(int feedbackId);
+        bool Exists(Feedback feedback);
+
+        void Create(Feedback feedback);
+
+        IEnumerable<Feedback> GetAllFeedbacksForLesson(Lesson lesson);
+        IEnumerable<Feedback> GetAllFeedbacksWrittenByUser(ApplicationUser user);
+        IEnumerable<Feedback> GetAllFeedbacksUnreadByUser(ApplicationUser user);
+    }
+}
