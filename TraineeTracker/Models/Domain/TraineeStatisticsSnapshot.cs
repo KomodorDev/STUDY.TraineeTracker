6666cd844f226bf48c900349eeadb5b3d3491b15
stefanni <nikita.stefan@uni-a.de>
2025-06-19 12:56:41 +0200

    Aufsetzen von TraineeStatisticsSnapshot.cs (Model)

---
+++ TraineeTracker/Models/Domain/TraineeStatisticsSnapshot.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace TraineeTracker.Models
+{
+    public class TraineeStatisticsSnapshot
+    {
+        public string userId { get; set; }
+
+        // das Datum von dem Snapshot zu den einzelnen Kennzahlen
+        public DateTime SnapshotDate { get; set; }
+
+        //entspricht der Kennzahl "Anwesend"
+        public double DaysPresent { get; set; }
+
+        //entspricht der Kennzahl "Geschafft"
+        public double LessonDaysCompleted { get; set; }
+
+        //entspricht der Kennzahl "Offen"
+        public double LessonDaysOpen { get; set; }
+
+        //entspricht der Kennzahl "Puffer"
+        public double LessonDaysBuffer { get; set; }
+
+        //entspricht der Kennzahl "Geschwindigkeit"
+        public double Speed { get; set; }
+
+        //entspricht der Kennzahl "Puffer Vorhersage"
+        public double DaysBufferPredicted { get; set; }
+    }
+}
