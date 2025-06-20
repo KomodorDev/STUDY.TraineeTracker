24cfb5345cf0eaba33081062057ef7b7741f3d3c
KomodorDev <sah_services@outlook.com>
2025-06-20 12:47:34 +0200

    Exists and GetByUserId Methoden für Repo hinzugefügt

--- TraineeTracker/Data/EmailNotificationSettings/DatabaseEmailNotificationSettingRepository.cs
+++ TraineeTracker/Data/EmailNotificationSettings/DatabaseEmailNotificationSettingRepository.cs
@@ -2,31 +2,36 @@ using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Data
 {
-    public class DatabaseEmailNotificationSettingRepository : IEmailNotificationSettingRepository
-    {
+    public class DatabaseEmailNotificationSettingRepository : IEmailNotificationSettingRepository {
         private readonly ApplicationDbContext _context;
 
-        public DatabaseEmailNotificationSettingRepository(ApplicationDbContext context)
-        {
+        public DatabaseEmailNotificationSettingRepository(ApplicationDbContext context) {
             _context = context;
         }
 
-        public void Create(EmailNotificationSetting emailNotificationSetting)
-        {
+        public void Create(EmailNotificationSetting emailNotificationSetting) {
             _context.EmailNotificationSettings.Add(emailNotificationSetting);
             _context.SaveChanges();
         }
 
-        public void Update(EmailNotificationSetting emailNotificationSetting)
-        {
+        public void Update(EmailNotificationSetting emailNotificationSetting) {
             _context.EmailNotificationSettings.Update(emailNotificationSetting);
             _context.SaveChanges();
         }
 
-        public void Delete(EmailNotificationSetting emailNotificationSetting)
-        {
+        public void Delete(EmailNotificationSetting emailNotificationSetting) {
             _context.EmailNotificationSettings.Remove(emailNotificationSetting);
             _context.SaveChanges();
+        }
+        
+        public bool Exists(string userId)
+        {
+            return _context.EmailNotificationSettings.Any(s => s.UserId == userId);
+        }
+
+        public EmailNotificationSetting? GetByUserId(string userId) {
+            return _context.EmailNotificationSettings
+                .FirstOrDefault(s => s.UserId == userId);
         }
     }
 }
