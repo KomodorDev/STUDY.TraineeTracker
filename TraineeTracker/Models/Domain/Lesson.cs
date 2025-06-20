2277e419a4aad04866b52419b4364c3cfb67375b
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-06-20 23:55:40 +0200

    Navigation zu TraineeLesson hinzugefügt

--- TraineeTracker/Models/Domain/Lesson.cs
+++ TraineeTracker/Models/Domain/Lesson.cs
@@ -1,19 +1,30 @@
 namespace TraineeTracker.Models.Domain
 {
     public class Lesson {
-        public required int LessonId { get; set; }
+        public required int LessonId {
+            get; set;
+        }
 
-        public required string Title { get; set; }
+        public required string Title {
+            get; set;
+        }
 
-        public required double EstimatedEffort { get; set; }
+        public required double EstimatedEffort {
+            get; set;
+        }
 
-        public required string LinkUrl { get; set; }
+        public required string LinkUrl {
+            get; set;
+        }
 
         public bool IsInactive { get; set; } = false;
 
         public ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
-    
+
+        public ICollection<TraineeLesson> TraineeLessons { get; set; } = new List<TraineeLesson>();
+
         public ICollection<TeachingPlan> TeachingPlans { get; set; } = new List<TeachingPlan>();
+        
     
 
     }
