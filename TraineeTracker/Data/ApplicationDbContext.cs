4a63e661ce2d9a1ec9c5baf604417c022cd0456f
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-06-20 13:52:03 +0200

    Changes in DbContext reversed

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using TraineeTracker.Models.Domain;
+
+namespace TraineeTracker.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-    
-    public DbSet<Feedback> Feedbacks { get; set; }
-
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) {
+        : base(options)
+    {
     }
 }
