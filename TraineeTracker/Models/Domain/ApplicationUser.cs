ddb25011ea1e319498c56067328132ae2d985911
highestPACE <pschweizer0711@gmail.com>
2025-06-20 12:59:08 +0200

    Added Properties of ApplicationUser

--- TraineeTracker/Models/Domain/ApplicationUser.cs
+++ TraineeTracker/Models/Domain/ApplicationUser.cs
@@ -1,7 +1,22 @@
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Identity;
 
 namespace TraineeTracker.Models.Domain {
     public class ApplicationUser : IdentityUser {
-        
+        public bool IsClosed {
+            get;
+            set;
+        } = false; // to be implemented
+
+        public DateTime? TraineeStartDate {
+            get;
+            set;
+        }
+        public DateTime? TraineeEndDate {
+            get;
+            set;
+        }
+        public List<ProcessingPause> ProcessingPauses;
+        public List<TraineeLesson> TraineeLessons;
     }
 }
