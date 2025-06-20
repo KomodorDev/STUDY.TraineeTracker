75bf23a601cd57fb8a129c414d8c75e20ba53813
schleale <alexander.schlemmer@uni-a.de>
2025-06-20 13:06:15 +0200

    Added TraineeLessons DbSet to DbContext

--- TraineeTracker/Data/ApplicationDbContext.cs
+++ TraineeTracker/Data/ApplicationDbContext.cs
@@ -3,10 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TraineeTracker.Data;
 
+using TraineeTracker.Models.Domain; // for TraineeLesson
+
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<TraineeLesson> TraineeLessons { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
+        : base(options) {
     }
 }
