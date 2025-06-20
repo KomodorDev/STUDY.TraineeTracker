76430a323e1161cf2eb0faf5315d5b9121777c86
blaskale <alexandros.blask@uni-a.de>
2025-06-20 12:32:01 +0200

    Ich habe nun die I-Repositories und die Database-Repositories hinzugefügt basierend auf dem Designklassendiagramm

---
+++ TraineeTracker/Data/TeachingPlans/DatabaseTeachingPlanRepository.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using TraineeTracker.Models.Domain;
+
+namespace TraineeTracker.Data.TeachingPlans
+{
+    public class DatabaseTeachingPlanRepository : ITeachingPlanRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DatabaseTeachingPlanRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Exists(int id)
+        {
+            return _context.TeachingPlans.Any(tp => tp.TeachingPlanId == id);
+        }
+
+        public bool Exists(TeachingPlan teachingPlan)
+        {
+            return _context.TeachingPlans.Any(tp =>
+            tp.Name == teachingPlan.Name &&
+            tp.LastUpdated == teachingPlan.LastUpdated);
+        }
+
+        public void Create(TeachingPlan teachingPlan)
+        {
+            _context.TeachingPlans.Add(teachingPlan);
+            _context.SaveChanges();
+        }
+
+        public void Update(TeachingPlan teachingPlan)
+        {
+            _context.TeachingPlans.Update(teachingPlan);
+            _context.SaveChanges();
+        }
+
+        public void Delete(TeachingPlan teachingPlan)
+        {
+            _context.TeachingPlans.Remove(teachingPlan);
+            _context.SaveChanges();
+        }
+
+        public Lesson GetLessonById(int id)
+        {
+            return _context.TeachingPlans
+            .SelectMany(tp => tp.Lessons)
+            .FirstOrDefault(l => l.LessonId == id);
+        }
+
+        public IEnumerable<TeachingPlan> GetAllTeachingPlans()
+        {
+            return _context.TeachingPlans.ToList();
+        }
+    }
+}
