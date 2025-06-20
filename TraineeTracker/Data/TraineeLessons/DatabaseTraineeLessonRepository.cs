6091d91bcd57aa5284b271bf9b998692ec08eb24
schleale <alexander.schlemmer@uni-a.de>
2025-06-20 13:52:34 +0200

    Added annotations

--- TraineeTracker/Data/TraineeLessons/DatabaseTraineeLessonRepository.cs
+++ TraineeTracker/Data/TraineeLessons/DatabaseTraineeLessonRepository.cs
@@ -36,21 +36,21 @@ namespace TraineeTracker.Data.TraineeLessons {
 
         public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfLesson(int lessonId) {
             return _context.TraineeLessons
-                .Include(t => t.Lesson)
+                .Include(t => t.Lesson)     // eager loads the Lesson for easier access to properties of the fitting lesson
                 .Where(tl => tl.LessonId == lessonId)
-                .ToList();
+                .ToList();                  // "give me all lessons now" -> loaded into memory; if lots of further sorting is required, remove.
         }
 
         public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfTrainee(string traineeId) {
             return _context.TraineeLessons
-                .Include(t => t.Lesson)
+                .Include(t => t.Lesson)     // eager loads the Lesson for easier access to properties of the fitting lesson
                 .Where(tl => tl.UserId == traineeId)
-                .ToList();
+                .ToList();                  // "give me all lessons now" -> loaded into memory; if lots of further sorting is required, remove.
         }
 
         public TraineeLesson? GetTraineeLessonById(int traineeLessonId) {
             return _context.TraineeLessons
-                .Include(t => t.Lesson)
+                .Include(t => t.Lesson)     // eager loads the Lesson for easier access to properties of the fitting lesson
                 .FirstOrDefault(tl => tl.TraineeLessonId == traineeLessonId);
         }
     }
