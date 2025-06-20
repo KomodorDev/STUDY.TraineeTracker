107bed7436d42a2ba06fe859d292edbf57f37b33
KomodorDev <sah_services@outlook.com>
2025-06-20 23:16:50 +0200

    List to ICollection abgeändert

--- TraineeTracker/Models/Domain/Lesson.cs
+++ TraineeTracker/Models/Domain/Lesson.cs
@@ -1,25 +1,17 @@
 namespace TraineeTracker.Models.Domain
 {
     public class Lesson {
-        public required int LessonId {
-            get; set;
-        }
+        public required int LessonId { get; set; }
 
-        public required string Title {
-            get; set;
-        }
+        public required string Title { get; set; }
 
-        public required double EstimatedEffort {
-            get; set;
-        }
+        public required double EstimatedEffort { get; set; }
 
-        public required string LinkUrl {
-            get; set;
-        }
+        public required string LinkUrl { get; set; }
 
         public bool IsInactive { get; set; } = false;
 
-        public List<Feedback> Feedbacks { get; set; } = new();
+        public ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
     
         public ICollection<TeachingPlan> TeachingPlans { get; set; } = new List<TeachingPlan>();
     
