cef2cd274ce43a2372ac57855218c7b850d67869
blaskale <alexandros.blask@uni-a.de>
2025-06-20 17:47:52 +0200

    Ich hatte einen Fehler bei der Benenung der Ordner wodurch es zu einem Fehler gab bei dem ein Typ mit einem Namespace verwechselt wurde sollte nun aber behoben sein

--- TraineeTracker/Data/ProcessingPauses/DatabaseProcessingPauserepository.cs
+++ TraineeTracker/Data/ProcessingPauses/DatabaseProcessingPauserepository.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using TraineeTracker.Models.Domain;
 
-namespace TraineeTracker.Data.ProcessingPause
+namespace TraineeTracker.Data.ProcessingPauses
 {
     public class DatabaseProcessingPauseRepository : IProcessingPauseRepository
     {
