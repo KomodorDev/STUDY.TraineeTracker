63742cc7f0bbf4bffdf3b760b712ba4026824aff
blaskale <alexandros.blask@uni-a.de>
2025-06-20 10:30:16 +0200

    Namespaces in Models gefixxed

--- TraineeTracker/Models/Domain/TeachingPlan.cs
+++ TraineeTracker/Models/Domain/TeachingPlan.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Tutorial_Project.Models
+namespace TraineeTracker.Models.Domain
 {
     public class TeachingPlan
     {
