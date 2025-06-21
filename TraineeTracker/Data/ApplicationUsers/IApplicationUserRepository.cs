1433cc7b4a1c8b5716a8d8fcc72d57cfd428ee1e
highestPACE <pschweizer0711@gmail.com>
2025-06-21 18:16:10 +0200

    UserManager durch ApplicationUserRepository für bessere Trennung ersetzt und dazu nötige Methoden zum ApplicationUserRepository hinzugefügt.

--- TraineeTracker/Data/ApplicationUsers/IApplicationUserRepository.cs
+++ TraineeTracker/Data/ApplicationUsers/IApplicationUserRepository.cs
@@ -1,14 +1,21 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Data.ApplicationUsers {
     public interface IApplicationUserRepository {
+        public Task AddToRoleAsync(ApplicationUser user, string role);
+
+        public Task CreateAsync(ApplicationUser user, string password);
+
         public Task<bool> ExistsAsync(int applicationUserId);
 
         public Task<bool> ExistsAsync(ApplicationUser applicationUser);
 
-        public Task<bool> RoleExistsAsync(string roleName);
+        public Task<ApplicationUser?> FindByEmailAsync(string email);
 
         public Task<IEnumerable<ApplicationUser>> GetUsersInRoleAsync(string roleName);
+
+        public Task<bool> IsInRoleAsync(ApplicationUser user, string role);
     }
 }
