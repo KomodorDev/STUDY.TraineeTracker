7c48550b74e7c24de15530b434092157f6eb56a0
highestPACE <pschweizer0711@gmail.com>
2025-06-21 19:17:16 +0200

    AdminService mit CreateUserAsync-Funktion hinzugefügt und in Program.cs registriert.

--- TraineeTracker/Program.cs
+++ TraineeTracker/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using TraineeTracker.Data;
 using TraineeTracker.Models.Domain;
 using TraineeTracker.Services.Seeders;
+using TraineeTracker.Services.Admin;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,7 +31,8 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-
+// Controller Services
+builder.Services.AddScoped<AdminService>();
 
 // ----------------------------------------
 // Register Email Service
