b93e5f174ffdcf81a45d82b31fc9c8699eabe294
highestPACE <pschweizer0711@gmail.com>
2025-06-21 19:20:20 +0200

    ApplicationUserDto hinzugefügt

---
+++ TraineeTracker/Models/Dtos/ApplicationUserDto.cs
@@ -0,0 +1,7 @@
+namespace TraineeTracker.Models.Dtos {
+    public class ApplicationUserDto {
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String Role { get; set; }
+    }
+}
