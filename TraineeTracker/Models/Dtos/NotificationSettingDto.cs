9a79beff22b9e46bb3aa075e5ceaeaeca5fe73ce
KomodorDev <sah_services@outlook.com>
2025-06-27 18:31:04 +0200

    UserId removed from Dto

--- TraineeTracker/Models/Dtos/NotificationSettingDto.cs
+++ TraineeTracker/Models/Dtos/NotificationSettingDto.cs
@@ -8,6 +8,6 @@ namespace TraineeTracker.Models.Dtos {
         public bool ReceiveAcceptedNotifications { get; set; }
         public bool ReceiveRatedNotifications { get; set; }
         public bool ReceiveImportChangeNotifications { get; set; }
-        public string? UserId { get; set; } // Optional, falls du das aus Claims ableitest
+
     }
 }
