5d305846d1404d47bdfc198c92d99f609d09f6f7
blaskale <alexandros.blask@uni-a.de>
2025-06-27 16:28:17 +0200

    Logiküberarbeitung in Service und Create, Update und Delete in LessonRepo auf async

--- TraineeTracker/Services/TeachingPlanService.cs
+++ TraineeTracker/Services/TeachingPlanService.cs
@@ -1,19 +1,29 @@
 using Newtonsoft.Json;
 using TraineeTracker.Data.TeachingPlans;
+using TraineeTracker.Data.TraineeLessons;
 using TraineeTracker.Models.Domain;
+using TraineeTracker.Models.Dto;
 
 namespace TraineeTracker.Services {
 
     public class TeachingPlanService {
-        private readonly ITeachingPlanRepository _teachingPlanRepo;
 
-        public TeachingPlanService(ITeachingPlanRepository teachingPlanRepo) {
+        private readonly ITeachingPlanRepository _teachingPlanRepo;
+        private readonly ILessonRepository _lessonRepo;
+        private readonly ITraineeLessonRepository _traineeLessonRepo;
+
+        public TeachingPlanService(ITeachingPlanRepository teachingPlanRepo, ILessonRepo lessonRepo) {
             _teachingPlanRepo = teachingPlanRepo;
+            _lessonRepo = lessonRepo;
         }
 
-        public async Task ImportNewTeachingPlan(IFormFile file) {
+        public async Task ImportNewTeachingPlan(IFormFile file, String name) {
+
             if (file == null || file.Length == 0)
                 throw new ArgumentException("Die Datei ist leer!");
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Ungültiger Name!");
 
             using var stream = new StreamReader(file.OpenReadStream());
             var jsonContent = await stream.ReadToEndAsync();
@@ -21,12 +31,29 @@ namespace TraineeTracker.Services {
             var lessonsDto = JsonConvert.DeserializeObject<List<LessonDto>>(jsonContent);
 
             if (lessonsDto == null || lessonsDto.Count == 0)
-                throw new InvalidOperationException("Keine gültigen Lektionen im JSON gefunden.");
+                throw new InvalidOperationException("Keine gültigen Lektionen im JSON gefunden!");
+
+            var lessons = lessonsDto.Select(dto => new Lesson
+            {
+                LessonId = dto.Id,
+                Title = dto.Title,
+                LinkUrl = dto.Url,
+                EstimatedEffort = dto.Estimate ?? 0,
+                IsInactive = dto.Deprecated
+            }).ToList();
+
+            var teachingPlan = new TeachingPlan
+            {
+                Name = name,
+                LastUpdated = DateTime.UtcNow,
+                Lessons = lessons
+            };
 
             await _teachingPlanRepo.Create(teachingPlan);
         }
 
         public async Task UpdateTeachingPlan(IFormFile file) {
+
             if (file == null || file.Length == 0)
                 throw new ArgumentException("Die Datei ist leer!");
 
@@ -37,6 +64,30 @@ namespace TraineeTracker.Services {
 
             if (lessonsDto == null || lessonsDto.Count == 0)
                 throw new InvalidOperationException("Keine gültigen Lektionen im JSON gefunden.");
+
+            var lessons = lessonsDto.Select(dto => new Lesson
+            {
+                LessonId = dto.Id,
+                Title = dto.Title,
+                LinkUrl = dto.Url,
+                EstimatedEffort = dto.Estimate ?? 0,
+                IsInactive = dto.Deprecated
+            }).ToList();
+
+            foreach(var lesson in lessons){
+
+                var traineeLesson = _traineeLessonRepo.GetTraineeLessonById(lesson.LessonId);
+
+                if(_lessonRepo.GetLessonById(lesson.LessonId) != null){
+                    if(lesson.IsInactive == true){
+                        if(traineeLesson.TraineeLessonState == "open"){
+                            await _traineeLessonRepo.Delete(lesson.LessonId);
+                        }
+                    }else{
+                        await _lessonRepo.Update(lesson);
+                    }
+                }
+            }
 
             await _teachingPlanRepo.Update(teachingPlan);
         }
