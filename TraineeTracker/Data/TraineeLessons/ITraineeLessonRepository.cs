a891fbf1077dd6ed565f4bb40392be86f9a74ac3
blaskale <alexandros.blask@uni-a.de>
2025-06-27 16:28:17 +0200

    Logiküberarbeitung in Service und Create, Update und Delete in LessonRepo auf async

--- TraineeTracker/Data/TraineeLessons/ITraineeLessonRepository.cs
+++ TraineeTracker/Data/TraineeLessons/ITraineeLessonRepository.cs
@@ -4,9 +4,9 @@ namespace TraineeTracker.Data.TraineeLessons {
     public interface ITraineeLessonRepository {
         public bool Exists(int traineeLessonId);
         public bool Exists(TraineeLesson traineeLesson);
-        public void Create(TraineeLesson traineeLesson);
-        public void CreateRange(IEnumerable<TraineeLesson> traineeLessons);
-        public void Update(TraineeLesson traineeLesson);
+        public async Task Create(TraineeLesson traineeLesson);
+        public async Task CreateRange(IEnumerable<TraineeLesson> traineeLessons);
+        public async Task Update(TraineeLesson traineeLesson);
         public TraineeLesson? GetTraineeLessonById(int traineeLessonId);
         public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfTrainee(string traineeId);
         public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfLesson(int lessonId);
