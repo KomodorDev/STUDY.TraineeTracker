aa0da3ed032c72d10a53196c2085b5dc5d5da20a
highestPACE <pschweizer0711@gmail.com>
2025-06-27 17:16:24 +0200

    ServiceResult abgeändert, sodass nun mehrere Fehlermeldungen gespeichert werden können

--- TraineeTracker/Services/ServiceResult.cs
+++ TraineeTracker/Services/ServiceResult.cs
@@ -3,13 +3,14 @@ namespace TraineeTracker.Models.Dtos {
     // Summary:
     //     Repräsentiert das Ergebnis einer Service Operation
     public class ServiceResult {
-        public bool Succeeded { get; protected set; }
-        public string ErrorMessage { get; protected set; } = "";
+        public bool Succeeded { get; private set; }
+
+        public IEnumerable<string> ErrorMessages { get; private set; } = new List<string>();
 
         public static ServiceResult Success() => new ServiceResult { Succeeded = true };
-        public static ServiceResult Failed(string errorMessage) => new ServiceResult {
+        public static ServiceResult Failed(params string[] errorMessages) => new ServiceResult {
             Succeeded = false,
-            ErrorMessage = errorMessage
+            ErrorMessages = errorMessages.ToList()
         };
     }
 }
