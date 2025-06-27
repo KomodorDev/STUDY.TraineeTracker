f29abc758541d3feead4e7ce172fc3463c701bd6
schleale <alexander.schlemmer@uni-a.de>
2025-06-27 17:02:06 +0200

    Made Repository methods async

--- TraineeTracker/Data/TraineeLessons/DatabaseTraineeLessonRepository.cs
+++ TraineeTracker/Data/TraineeLessons/DatabaseTraineeLessonRepository.cs
@@ -10,47 +10,47 @@ namespace TraineeTracker.Data.TraineeLessons {
             _context = context;
         }
 
-        public void Create(TraineeLesson traineeLesson) {
-            _context.TraineeLessons.Add(traineeLesson);
-            _context.SaveChanges();
+        public async Task Create(TraineeLesson traineeLesson) {
+            await _context.TraineeLessons.AddAsync(traineeLesson);
+            await _context.SaveChangesAsync();
         }
 
-        public void CreateRange(IEnumerable<TraineeLesson> traineeLessons) {
-            _context.AddRange(traineeLessons);
-            _context.SaveChanges();
+        public async Task CreateRange(IEnumerable<TraineeLesson> traineeLessons) {
+            await _context.AddRangeAsync(traineeLessons);
+            await _context.SaveChangesAsync();
         }
 
-        public bool Exists(int traineeLessonId) {
-            return _context.TraineeLessons.Any(tl => tl.TraineeLessonId == traineeLessonId);
+        public async Task<bool> Exists(int traineeLessonId) {
+            return await _context.TraineeLessons.AnyAsync(tl => tl.TraineeLessonId == traineeLessonId);
         }
 
-        public bool Exists(TraineeLesson traineeLesson) {
-            return _context.TraineeLessons.Any(tl => tl.TraineeLessonId == traineeLesson.TraineeLessonId);
+        public async Task<bool> Exists(TraineeLesson traineeLesson) {
+            return await _context.TraineeLessons.AnyAsync(tl => tl.TraineeLessonId == traineeLesson.TraineeLessonId);
         }
 
-        public void Update(TraineeLesson traineeLesson) {
-            _context.TraineeLessons.Update(traineeLesson);
-            _context.SaveChanges();
+        public async Task Update(TraineeLesson traineeLesson) {
+            _context.TraineeLessons.Update(traineeLesson);      // Update is not async
+            await _context.SaveChangesAsync();
         }
 
-        public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfLesson(int lessonId) {
-            return _context.TraineeLessons
+        public async Task<IEnumerable<TraineeLesson>> GetAllTraineeLessonsOfLesson(int lessonId) {
+            return await _context.TraineeLessons
                 .Include(t => t.Lesson)     // eager loads the Lesson for easier access to properties of the fitting lesson
                 .Where(tl => tl.LessonId == lessonId)
-                .ToList();                  // "give me all lessons now" -> loaded into memory; if lots of further sorting is required, remove.
+                .ToListAsync();                  // "give me all lessons now" -> loaded into memory; if lots of further sorting is required, remove.
         }
 
-        public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfTrainee(string traineeId) {
-            return _context.TraineeLessons
+        public async Task<IEnumerable<TraineeLesson>> GetAllTraineeLessonsOfTrainee(string traineeId) {
+            return await _context.TraineeLessons
                 .Include(t => t.Lesson)     // eager loads the Lesson for easier access to properties of the fitting lesson
                 .Where(tl => tl.UserId == traineeId)
-                .ToList();                  // "give me all lessons now" -> loaded into memory; if lots of further sorting is required, remove.
+                .ToListAsync();                  // "give me all lessons now" -> loaded into memory; if lots of further sorting is required, remove.
         }
 
-        public TraineeLesson? GetTraineeLessonById(int traineeLessonId) {
-            return _context.TraineeLessons
+        public async Task<TraineeLesson?> GetTraineeLessonById(int traineeLessonId) {
+            return await _context.TraineeLessons
                 .Include(t => t.Lesson)     // eager loads the Lesson for easier access to properties of the fitting lesson
-                .FirstOrDefault(tl => tl.TraineeLessonId == traineeLessonId);
+                .FirstOrDefaultAsync(tl => tl.TraineeLessonId == traineeLessonId);
         }
     }
 }
--- TraineeTracker/Data/TraineeLessons/ITraineeLessonRepository.cs
+++ TraineeTracker/Data/TraineeLessons/ITraineeLessonRepository.cs
@@ -2,13 +2,13 @@ using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Data.TraineeLessons {
     public interface ITraineeLessonRepository {
-        public bool Exists(int traineeLessonId);
-        public bool Exists(TraineeLesson traineeLesson);
-        public void Create(TraineeLesson traineeLesson);
-        public void CreateRange(IEnumerable<TraineeLesson> traineeLessons);
-        public void Update(TraineeLesson traineeLesson);
-        public TraineeLesson? GetTraineeLessonById(int traineeLessonId);
-        public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfTrainee(string traineeId);
-        public IEnumerable<TraineeLesson> GetAllTraineeLessonsOfLesson(int lessonId);
+        Task<bool> Exists(int traineeLessonId);
+        Task<bool> Exists(TraineeLesson traineeLesson);
+        Task Create(TraineeLesson traineeLesson);
+        Task CreateRange(IEnumerable<TraineeLesson> traineeLessons);
+        Task Update(TraineeLesson traineeLesson);
+        Task<TraineeLesson?> GetTraineeLessonById(int traineeLessonId);
+        Task<IEnumerable<TraineeLesson>> GetAllTraineeLessonsOfTrainee(string traineeId);
+        Task<IEnumerable<TraineeLesson>> GetAllTraineeLessonsOfLesson(int lessonId);
     }
 }
