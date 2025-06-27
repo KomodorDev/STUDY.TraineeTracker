7873ca9ae032aa5fcaad14047ff60cbae79d369b
highestPACE <pschweizer0711@gmail.com>
2025-06-27 17:32:28 +0200

    AdminController und -Service an neues ServiceResult angepasst

--- TraineeTracker/Services/AdminService.cs
+++ TraineeTracker/Services/AdminService.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using TraineeTracker.Data.ApplicationUsers;
 using TraineeTracker.Data.ProcessingPauses;
+using TraineeTracker.Data.TeachingPlans;
 using TraineeTracker.Models.Domain;
 using TraineeTracker.Models.Dtos;
 
@@ -8,24 +9,31 @@ namespace TraineeTracker.Services.Admin {
     public class AdminService {
         private readonly IApplicationUserRepository _applicationUserRepository;
         private readonly IProcessingPauseRepository _processingPauseRepository;
+        private readonly ITeachingPlanRepository _teachingPlanRepository;
 
-        public AdminService(IApplicationUserRepository applicationUserRepository, IProcessingPauseRepository processingPauseRepository) {
+        public AdminService(IApplicationUserRepository applicationUserRepository, IProcessingPauseRepository processingPauseRepository, ITeachingPlanRepository teachingPlanRepository) {
             _applicationUserRepository = applicationUserRepository;
             _processingPauseRepository = processingPauseRepository;
+            _teachingPlanRepository = teachingPlanRepository;
         }
 
         // Hier fehlen die TraineeLessons und die NotificationSettings
-        public async Task<IdentityResult> CreateUserAsync(ApplicationUserDto dto) {
+        public async Task<ServiceResult> CreateUserAsync(ApplicationUserDto dto) {
             var user = new ApplicationUser {
                 UserName = dto.Email,
                 Email = dto.Email,
                 EmailConfirmed = true
             };
             var result = await _applicationUserRepository.CreateAsync(user, dto.Password);
-            if (result.Succeeded) {
-                await _applicationUserRepository.AddToRoleAsync(user, dto.Role);
+            if (!result.Succeeded) {
+                return ServiceResult.Failed(result.Errors.Select(e => e.Description).ToArray());
             }
-            return result;
+            var roleResult = await _applicationUserRepository.AddToRoleAsync(user, dto.Role);
+            if (!roleResult.Succeeded) {
+                var errors = result.Errors.Concat(roleResult.Errors);
+                return ServiceResult.Failed(errors.Select(e => e.Description).ToArray());
+            }
+            return ServiceResult.Success();
         }
 
         public async Task<bool> SetIsClosedAsync(string userId, bool isClosed) {
