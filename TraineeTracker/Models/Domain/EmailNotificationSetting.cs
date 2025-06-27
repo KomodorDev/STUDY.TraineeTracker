1c385a434d938618568934776f8cfd34c45dcde5
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-06-27 13:40:14 +0200

    Default values for EmailNotificationSettings

--- TraineeTracker/Models/Domain/EmailNotificationSetting.cs
+++ TraineeTracker/Models/Domain/EmailNotificationSetting.cs
@@ -1,18 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace TraineeTracker.Models.Domain
-{
-    public class EmailNotificationSetting
-    {
+namespace TraineeTracker.Models.Domain {
+    public class EmailNotificationSetting {
         [Key]
         public int EmailNotificationSettingsId { get; set; }
 
-        public bool ReceiveOpenNotifications { get; set; }
-        public bool ReceiveStartedNotifications { get; set; }
-        public bool ReceiveFinishedNotifications { get; set; }
-        public bool ReceiveRejectedNotifications { get; set; }
-        public bool ReceiveAcceptedNotifications { get; set; }
-        public bool ReceiveRatedNotifications { get; set; }
+        public bool ReceiveOpenNotifications { get; set; } = false;
+        public bool ReceiveStartedNotifications { get; set; } = false;
+        public bool ReceiveFinishedNotifications { get; set; } = false;
+        public bool ReceiveRejectedNotifications { get; set; } = false;
+        public bool ReceiveAcceptedNotifications { get; set; } = false;
+        public bool ReceiveRatedNotifications { get; set; } = false;
 
         // Foreign key
         public required string UserId { get; set; }
