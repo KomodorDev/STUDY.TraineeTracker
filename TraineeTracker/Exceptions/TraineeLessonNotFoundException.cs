66a0dcca501a08298520443034fb75156b037e47
schleale <alexander.schlemmer@uni-a.de>
2025-06-27 12:13:52 +0200

    Created TraineeLessonNotFoundException in new root level folder

---
+++ TraineeTracker/Exceptions/TraineeLessonNotFoundException.cs
@@ -0,0 +1,22 @@
+// class by schleale
+
+namespace TraineeTracker.Exceptions {
+    public class TraineeLessonNotFoundException : Exception {
+
+        // : base() passes parameters upward to Exception class
+        public TraineeLessonNotFoundException()
+            : base("The specified TraineeLesson could not be found.") {
+
+        }
+
+        public TraineeLessonNotFoundException(string message)
+            : base(message) {
+
+        }
+
+        public TraineeLessonNotFoundException(string message, Exception innerException)
+            : base(message, innerException) {
+
+        }
+    }
+}
