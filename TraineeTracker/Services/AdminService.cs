f480381e2a4ab4ea81c102b75ef1a1e2e2459071
highestPACE <pschweizer0711@gmail.com>
2025-06-27 18:23:43 +0200

    ApplicationUser werden nun bei Erstellung EmailNotificationSettings zugewiesen, sowie - wenn Trainee - einen Teachingplan mit den entsprechenden TraineeLessons

--- TraineeTracker/Services/AdminService.cs
+++ TraineeTracker/Services/AdminService.cs
@@ -1,7 +1,5 @@
-using Microsoft.AspNetCore.Identity;
 using TraineeTracker.Data.ApplicationUsers;
 using TraineeTracker.Data.ProcessingPauses;
-using TraineeTracker.Data.TeachingPlans;
 using TraineeTracker.Models.Domain;
 using TraineeTracker.Models.Dtos;
 
@@ -9,30 +7,50 @@ namespace TraineeTracker.Services.Admin {
     public class AdminService {
         private readonly IApplicationUserRepository _applicationUserRepository;
         private readonly IProcessingPauseRepository _processingPauseRepository;
-        private readonly ITeachingPlanRepository _teachingPlanRepository;
 
-        public AdminService(IApplicationUserRepository applicationUserRepository, IProcessingPauseRepository processingPauseRepository, ITeachingPlanRepository teachingPlanRepository) {
+        private readonly EmailNotificationSettingService _emailNotificationSettingService;
+        private readonly TeachingPlanService _teachingPlanService;
+
+        public AdminService(IApplicationUserRepository applicationUserRepository, IProcessingPauseRepository processingPauseRepository, IEmailNotificationSettingService emailNotificationSettingService, ITeachingPlanService teachingPlanService) {
             _applicationUserRepository = applicationUserRepository;
             _processingPauseRepository = processingPauseRepository;
-            _teachingPlanRepository = teachingPlanRepository;
+            _emailNotificationSettingService = emailNotificationSettingService;
+            _teachingPlanService = teachingPlanService;
         }
 
-        // Hier fehlen die TraineeLessons und die NotificationSettings
         public async Task<ServiceResult> CreateUserAsync(ApplicationUserDto dto) {
             var user = new ApplicationUser {
                 UserName = dto.Email,
                 Email = dto.Email,
-                EmailConfirmed = true
+                EmailConfirmed = true,
+                EmailNotificationSetting = _emailNotificationSettingService.CreateDefaultSettings(dto.Role)
             };
+
+            if (dto.Role == "Trainee") {
+                if (dto.TeachingPlanId == null) {
+                    return ServiceResult.Failed("Teachingplan required for Trainee.");
+                }
+                var teachingPlan = _teachingPlanService.GetTeachingPlanByIdAsync(dto.TeachingPlanId);
+                if (teachingPlan == null) {
+                    return ServiceResult.Failed("Teachingplan not found.");
+                }
+                user.TeachingPlan = teachingPlan;
+                
+                var traineeLessons = await _teachingPlanService.CreateTraineeLessonsAsync(user, dto.TeachingPlanId);
+                user.TraineeLessons = traineeLessons;
+            }
+
             var result = await _applicationUserRepository.CreateAsync(user, dto.Password);
             if (!result.Succeeded) {
                 return ServiceResult.Failed(result.Errors.Select(e => e.Description).ToArray());
             }
+
             var roleResult = await _applicationUserRepository.AddToRoleAsync(user, dto.Role);
             if (!roleResult.Succeeded) {
                 var errors = result.Errors.Concat(roleResult.Errors);
                 return ServiceResult.Failed(errors.Select(e => e.Description).ToArray());
             }
+
             return ServiceResult.Success();
         }
 
