f4931196bc4de7f686b2dcdcc9ce7f7908fe7b8f
highestPACE <pschweizer0711@gmail.com>
2025-06-27 16:12:26 +0200

    Reihenfolge der Properties im ApplicationUser überarbeitet

--- TraineeTracker/Models/Domain/ApplicationUser.cs
+++ TraineeTracker/Models/Domain/ApplicationUser.cs
@@ -3,19 +3,16 @@ using Microsoft.AspNetCore.Identity;
 namespace TraineeTracker.Models.Domain {
     public class ApplicationUser : IdentityUser {
         public bool IsClosed { get; set; } = false; // to be implemented
-
+        public EmailNotificationSetting? EmailNotificationSetting { get; set; }
+        
         public DateTime? TraineeStartDate { get; set; }
         public DateTime? TraineeEndDate { get; set; }
-
-
-        public ICollection<ProcessingPause> ProcessingPauses { get; set; } = new List<ProcessingPause>();
-        public ICollection<TraineeLesson> TraineeLessons { get; set; } = new List<TraineeLesson>();
-        public TraineeStatisticsSnapshot? TraineeStatisticsSnapshot { get; set; }
         public TeachingPlan? TeachingPlan { get; set; }
+        public TraineeStatisticsSnapshot? TraineeStatisticsSnapshot { get; set; }
+
         public ICollection<Feedback> ReadFeedbacks { get; set; } = new List<Feedback>();
         public ICollection<Feedback> WrittenFeedbacks { get; set; } = new List<Feedback>();
-        public EmailNotificationSetting? EmailNotificationSetting { get; set; }
-
-
+        public ICollection<ProcessingPause> ProcessingPauses { get; set; } = new List<ProcessingPause>();
+        public ICollection<TraineeLesson> TraineeLessons { get; set; } = new List<TraineeLesson>();
     }
 }
