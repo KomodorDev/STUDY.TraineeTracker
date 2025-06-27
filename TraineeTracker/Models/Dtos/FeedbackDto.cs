a8b18e7cdc04869fb755ac6a0be84bb7ab10c9fc
schleale <alexander.schlemmer@uni-a.de>
2025-06-27 15:47:11 +0200

    Fixed FeedbackDto to be in line with Feedback model

--- TraineeTracker/Models/Dtos/FeedbackDto.cs
+++ TraineeTracker/Models/Dtos/FeedbackDto.cs
@@ -1,10 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TraineeTracker.Models.Dtos
 {
     public class FeedbackDto {
-        public required int TraineeLessonId { get; set; }
-        public int EffortDays { get; set; }
-        public string? PriorKnowledge { get; set; }
-        public string? Difficulty { get; set; }
-        public string? Comment { get; set; }
+        [Range(1, 10)]
+        public int? Difficulty {
+            get; set;
+        }
+
+        public string? PreviousKnowledge {
+            get; set;
+        }
+
+        public float? HoursOfEffort {
+            get; set;
+        }
+
+        public string? Comment {
+            get; set;
+        }
+        
+        public required int TraineeLessonId {
+            get; set;
+        }
     }
 }
