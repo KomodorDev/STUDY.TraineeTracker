d4d0c44464cc26cf376c942f6a0fd26df6e1e24b
schleale <alexander.schlemmer@uni-a.de>
2025-06-27 12:20:16 +0200

    Created LessonNotFoundException

---
+++ TraineeTracker/Exceptions/TraineeLessonNotFoundException copy.cs
@@ -0,0 +1,22 @@
+// class by schleale
+
+namespace TraineeTracker.Exceptions {
+    public class LessonNotFoundException : Exception {
+
+        // : base() passes parameters upward to Exception class
+        public LessonNotFoundException()
+            : base("The specified Lesson could not be found.") {
+
+        }
+
+        public LessonNotFoundException(string message)
+            : base(message) {
+
+        }
+
+        public LessonNotFoundException(string message, Exception innerException)
+            : base(message, innerException) {
+
+        }
+    }
+}
