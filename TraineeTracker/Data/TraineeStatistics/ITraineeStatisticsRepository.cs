cedc81b5555c9ca88871d0099347c77093439f54
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-06-28 18:51:15 +0200

    Async

--- TraineeTracker/Data/TraineeStatistics/ITraineeStatisticsRepository.cs
+++ TraineeTracker/Data/TraineeStatistics/ITraineeStatisticsRepository.cs
@@ -2,11 +2,11 @@ using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Data.TraineeStatistics {
     public interface ITraineeStatisticsRepository {
-        Task<bool> Exists(int id);
-        Task<bool> Exists(TraineeStatisticsSnapshot snapshot);
-        Task Create(TraineeStatisticsSnapshot snapshot);
-        Task Update(TraineeStatisticsSnapshot snapshot);
-        Task Delete(TraineeStatisticsSnapshot snapshot);
-        Task<TraineeStatisticsSnapshot> GetTraineeStatisticsSnapshot(string traineeId);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> ExistsAsync(TraineeStatisticsSnapshot snapshot);
+        Task CreateAsync(TraineeStatisticsSnapshot snapshot);
+        Task UpdateAsync(TraineeStatisticsSnapshot snapshot);
+        Task DeleteAsync(TraineeStatisticsSnapshot snapshot);
+        Task<TraineeStatisticsSnapshot> GetTraineeStatisticsSnapshotAsync(string traineeId);
     }
 }
