aca6a0ec70acddcace48d23cb09d6704dc027da2
schleale <alexander.schlemmer@uni-a.de>
2025-06-28 21:19:41 +0200

    Created VewModel

---
+++ TraineeTracker/Models/ViewModels/TraineeLessonDashboardViewModel.cs
@@ -0,0 +1,12 @@
+using TraineeTracker.Models.Domain;
+
+namespace TraineeTracker.Models.ViewModels
+{
+    public class TraineeLessonDashboardViewModel {
+        public TraineeStatisticsSnapshot? TraineeStatisticsSnapshot { get; set; }
+        public IEnumerable<Lesson>? Lessons { get; set; }
+        public IEnumerable<TraineeLesson>? TraineeLessons { get; set; }
+        public TeachingPlan? TeachingPlan { get; set; }
+
+    }
+}
