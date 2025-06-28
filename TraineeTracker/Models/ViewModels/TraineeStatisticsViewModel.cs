1720b26d416d87a7fea7779927b3882869045448
stefanni <nikita.stefan@uni-a.de>
2025-06-28 18:35:20 +0200

    Listen hinzugefügt für Statistik

--- TraineeTracker/Models/ViewModels/TraineeStatisticsViewModel.cs
+++ TraineeTracker/Models/ViewModels/TraineeStatisticsViewModel.cs
@@ -1,13 +1,20 @@
+using TraineeTracker.Models.Domain;
+
 namespace TraineeTracker.Models.ViewModels
 {
     public class TraineeStatisticsViewModel
     {
-        public DateTime SnapshotDate { get; set; }
+        public DateTime SnapshotDateTime { get; set; }
         public double DaysPresent { get; set; }
         public double LessonDaysCompleted { get; set; }
         public double LessonDaysOpen { get; set; }
         public double LessonDaysBuffer { get; set; }
         public double Speed { get; set; }
         public double DaysBufferPredicted { get; set; }
+        public List<TraineeLesson>? FinishedLessons { get; set; }
+        public List<TraineeLesson>? AcceptedAndRatedLessons { get; set; }
+        public List<TraineeLesson>? RejectedLessons { get; set; }
+        public List<TraineeLesson>? OpenLessons { get; set; }
+        public List<TraineeLesson>? StartedLessons { get; set; }
     }
 }
