977f387ba57da8cbaaec56a4b1b1dc96bd60f18d
schleale <alexander.schlemmer@uni-a.de>
2025-06-29 15:00:02 +0200

    Updated Dashboard ViewModel

--- TraineeTracker/Models/ViewModels/TraineeLessonDashboardViewModel.cs
+++ TraineeTracker/Models/ViewModels/TraineeLessonDashboardViewModel.cs
@@ -3,9 +3,10 @@ using TraineeTracker.Models.Domain;
 namespace TraineeTracker.Models.ViewModels
 {
     public class TraineeLessonDashboardViewModel {
+        public ApplicationUser? SelectedTrainee { get; set; }
         public TraineeStatisticsSnapshot? TraineeStatisticsSnapshot { get; set; }
         public IEnumerable<TraineeLesson>? TraineeLessons { get; set; }
-        public TeachingPlan? TeachingPlan { get; set; }
 
+        public IEnumerable<ApplicationUser>? Trainees { get; set; }
     }
 }
