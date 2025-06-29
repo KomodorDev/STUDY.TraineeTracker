b0b3353b0cf948660aad3f188f0eef6dffe9e252
highestPACE <pschweizer0711@gmail.com>
2025-06-29 15:54:59 +0200

    Required Properties nicht mehr nullable

--- TraineeTracker/Models/Dtos/ApplicationUserDto.cs
+++ TraineeTracker/Models/Dtos/ApplicationUserDto.cs
@@ -3,13 +3,13 @@ using System.ComponentModel.DataAnnotations;
 namespace TraineeTracker.Models.Dtos {
     public class ApplicationUserDto {
         [Required]
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; }
 
         [Required]
-        public string? Role { get; set; }
+        public string Role { get; set; }
         public int? TeachingPlanId { get; set; }
 
         // Only for Trainees
