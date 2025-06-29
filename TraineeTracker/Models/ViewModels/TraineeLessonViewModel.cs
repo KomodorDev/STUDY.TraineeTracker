84a0d46208a349fcd833254fb79514ffa3781e35
stefanni <nikita.stefan@uni-a.de>
2025-06-29 17:23:41 +0200

    TraineeLessonViewModel erstellt

---
+++ TraineeTracker/Models/ViewModels/TraineeLessonViewModel.cs
@@ -0,0 +1,14 @@
+using TraineeTracker.Models.Domain;
+using System;
+
+namespace TraineeTracker.Models.ViewModels
+{
+    public class TraineeLessonViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public double EstimatedEffort { get; set; }
+        public double WeightedEffort { get; set; }
+        public TraineeLessonState State { get; set; }
+    }
+}
+
