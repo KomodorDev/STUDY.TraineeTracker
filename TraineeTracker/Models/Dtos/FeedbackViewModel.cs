35ba1aa20a919c5927f86028773ed88a8f5dc60f
blaskale <alexandros.blask@uni-a.de>
2025-06-29 12:11:58 +0200

    Alles Falsch

--- TraineeTracker/Models/Dtos/FeedbackViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TraineeTracker.Models.Dtos
-{
-    public class FeedbackViewModel
-    {
-        public DateTime SentAt     { get; set; }
-        public string   AuthorName { get; set; } = string.Empty;
-        public string   Comment    { get; set; } = string.Empty;
-    }
-}
