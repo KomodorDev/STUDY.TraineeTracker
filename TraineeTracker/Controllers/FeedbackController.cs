cbf31c7b611ae3d2fbeee7c2530460f49e5d1bb5
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-07-02 11:59:00 +0200

    Formatting

--- TraineeTracker/Controllers/FeedbackController.cs
+++ TraineeTracker/Controllers/FeedbackController.cs
@@ -4,53 +4,50 @@ using Microsoft.AspNetCore.Mvc;
 using TraineeTracker.Models.ViewModels;
 using TraineeTracker.Services;
 
-namespace TraineeTracker.Controllers
-{
+namespace TraineeTracker.Controllers {
     [Authorize]
-    public class FeedbackController : Controller
-    {
+    public class FeedbackController : Controller {
         private readonly FeedbackService _feedbackService;
 
-        public FeedbackController(FeedbackService feedbackService)
-        {
+        // ------------------------------------------------------
+        public FeedbackController(FeedbackService feedbackService) {
             _feedbackService = feedbackService;
         }
 
+        // ------------------------------------------------------
         // GET: /Feedback/Dashboard
         [Route("FeedbackDashboard")]
         [HttpGet]
-        public async Task<IActionResult> ShowFeedbackDashboardView()
-        {
+        public async Task<IActionResult> ShowFeedbackDashboardView() {
             var viewModel = await _feedbackService.BuildFeedbackDashboardViewModelAsync(User);
-            return View("FeedbackDashboard",viewModel);
+            return View("FeedbackDashboard", viewModel);
         }
 
+        // ------------------------------------------------------
         // GET: /Feedback/All?page=1
-        public async Task<IActionResult> All(int page = 1)
-        {
+        public async Task<IActionResult> All(int page = 1) {
             var allPage = await _feedbackService.GetAllFeedbacksAsync(page);
             return View(allPage);
         }
 
+        // ------------------------------------------------------
         // GET: /Feedback/Unread?page=1
-        public async Task<IActionResult> Unread(int page = 1)
-        {
+        public async Task<IActionResult> Unread(int page = 1) {
             var unreadPage = await _feedbackService.GetUnreadFeedbacksAsync(User, page);
             return View(unreadPage);
         }
-
+        // ------------------------------------------------------
         // GET: /Feedback/Read?page=1
-        public async Task<IActionResult> Read(int page = 1)
-        {
+        public async Task<IActionResult> Read(int page = 1) {
             var readPage = await _feedbackService.GetReadFeedbacksAsync(User, page);
             return View(readPage);
         }
 
+        // ------------------------------------------------------
         // POST: /Feedback/MarkAsRead
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> MarkAsRead(int feedbackId, int page = 1)
-        {
+        public async Task<IActionResult> MarkAsRead(int feedbackId, int page = 1) {
             await _feedbackService.MarkFeedbackAsReadAsync(User, feedbackId);
 
             // 1) Referer-Header auslesen
@@ -63,5 +60,8 @@ namespace TraineeTracker.Controllers
             // Fallback: Dashboard
             return RedirectToAction(nameof(ShowFeedbackDashboardView));
         }
+
+        // ------------------------------------------------------
+
     }
 }
