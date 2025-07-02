018095250abaf2119f3832c16c2f0b1448e4b84e
highestPACE <pschweizer0711@gmail.com>
2025-07-02 21:37:54 +0200

    Add ProcessingPauseId property to ProcessingPauseDto for enhanced data handling

--- TraineeTracker/Models/Dtos/ProcessingPauseDto.cs
+++ TraineeTracker/Models/Dtos/ProcessingPauseDto.cs
@@ -3,6 +3,8 @@ using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Models.Dtos {
     public class ProcessingPauseDto {
+        public int? ProcessingPauseId { get; set; }
+
         [Required]
         public string TraineeId { get; set; } = "";
 
