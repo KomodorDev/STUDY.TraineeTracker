44d5042bd1055adb1d69cfbb20af47e377cdffe3
KomodorDev <sah_services@outlook.com>
2025-07-02 21:36:23 +0200

    Formatting and new attributes

--- TraineeTracker/Models/Dtos/FeedbackDashboardDto.cs
+++ TraineeTracker/Models/Dtos/FeedbackDashboardDto.cs
@@ -1,11 +1,23 @@
-namespace TraineeTracker.Models.Dtos
-{
-    public class FeedbackDashboardDto
-    {
+using System;
+
+namespace TraineeTracker.Models.Dtos {
+    public class FeedbackDashboardDto {
         public int FeedbackId { get; set; }
+
+        public bool IsReadByCurrentUser { get; set; }
+
         public string AuthorName { get; set; } = null!;
-        public DateTime SendDate   { get; set; }
-        public string? Comment     { get; set; }
+
+        public string LessonTitle { get; set; } = null!;
+
+        public DateTime CreateTime { get; set; }
+
+        public string? Comment { get; set; }
+
+        public int? Difficulty { get; set; }
+
+        public string? PreviousKnowledge { get; set; }
+
+        public float? HoursOfEffort { get; set; }
     }
 }
-
