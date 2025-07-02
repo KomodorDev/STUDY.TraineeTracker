ddb64f20d532c6f6d630219b1400f6acfc956e81
highestPACE <pschweizer0711@gmail.com>
2025-07-02 18:06:59 +0200

    Properties von ProcessingPauseDto nicht mehr required (nur per Validation)

--- TraineeTracker/Models/Dtos/ProcessingPauseDto.cs
+++ TraineeTracker/Models/Dtos/ProcessingPauseDto.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Models.Dtos {
     public class ProcessingPauseDto {
-        public required string TraineeId { get; set; }
-        public required DateOnly StartDate { get; set; }
+        [Required]
+        public string TraineeId { get; set; }
 
-        public required DateOnly EndDate { get; set; }
+        [Required]
+        public DateOnly StartDate { get; set; }
+
+        [Required]
+        public DateOnly EndDate { get; set; }
     }
 }
