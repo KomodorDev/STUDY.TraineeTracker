3ac432ff27549010cf26d700e28ed1bd45aa25dc
KomodorDev <sah_services@outlook.com>
2025-07-04 16:12:03 +0200

    ImportDashboardViewModel erstellt

---
+++ TraineeTracker/Models/ViewModels/TeachingPlan/ImportDashboardViewModel.cs
@@ -0,0 +1,4 @@
+using TraineeTracker.Models.ViewModels.TeachingPlan;
+public class ImportDashboardViewModel {
+    public List<ExistingTeachingPlanViewModel> ExistingTeachingPlans { get; set; } = new();
+}
