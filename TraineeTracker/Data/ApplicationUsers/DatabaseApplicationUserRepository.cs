90cac4c4052888a93f64a360af189ae76f1a7caa
KomodorDev <sah_services@outlook.com>
2025-07-04 13:49:54 +0200

    Added FindByIdWithNotificationSettingAsync

--- TraineeTracker/Data/ApplicationUsers/DatabaseApplicationUserRepository.cs
+++ TraineeTracker/Data/ApplicationUsers/DatabaseApplicationUserRepository.cs
@@ -58,6 +58,13 @@ namespace TraineeTracker.Data.ApplicationUsers {
             return await _userManager.FindByIdAsync(userId);
         }
 
+        public async Task<ApplicationUser?> FindByIdWithNotificationSettingAsync(string userId)
+        {
+            return await _context.Users
+                .Include(u => u.EmailNotificationSetting)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
         public async Task<ApplicationUser?> FindByIdWithLastSelectedTraineesAsync(string userId) {
             return await _context.Users
             .Include(u => u.LastSelectedTrainees)
