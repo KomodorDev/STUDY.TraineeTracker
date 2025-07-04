352b0caee888fe30cc85a9fc609fd705c88ba2f0
stefanni <nikita.stefan@uni-a.de>
2025-07-04 17:17:31 +0200

    PredictedEffortEndX wieder entfernt da nicht notwendig

--- TraineeTracker/Models/ViewModels/TraineeStatisticsViewModel.cs
+++ TraineeTracker/Models/ViewModels/TraineeStatisticsViewModel.cs
@@ -11,7 +11,6 @@ namespace TraineeTracker.Models.ViewModels {
         public double? Speed { get; set; }
         public double? PredictedMissingEstimatedEffortAtEnd { get; set; }
         public double? PredictedMissingActualDays { get; set; }
-        public double? PredictedEffortEndX { get; set; }
         public bool IsUpToDate { get; set; }
         public List<TraineeLessonViewModel>? FinishedLessons { get; set; }
         public List<TraineeLessonViewModel>? AcceptedAndRatedLessons { get; set; }
