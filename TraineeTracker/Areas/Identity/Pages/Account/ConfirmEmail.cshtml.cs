96b40e1e4e053aebff30fd20086fc544b4ff0872
highestPACE <pschweizer0711@gmail.com>
2025-07-06 19:55:47 +0200

    Ternären Operator durch If-Else-Statement ersetzt

--- TraineeTracker/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ TraineeTracker/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -45,7 +45,11 @@ namespace TraineeTracker.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            if (result.Succeeded) {
+                StatusMessage = "Thank you for confirming your email.";
+            } else {
+                StatusMessage = "Error confirming your email.";
+            }
             return Page();
         }
     }
