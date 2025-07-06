84178e2049923591309a5dd6f53b57fae5b43ea8
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-07-06 12:52:57 +0200

    Added MakandraID and TeachingPlanId for import identification. Also SortingIndex

--- TraineeTracker/Models/Domain/Lesson.cs
+++ TraineeTracker/Models/Domain/Lesson.cs
@@ -1,6 +1,17 @@
 namespace TraineeTracker.Models.Domain {
     public class Lesson {
-        public required int LessonId { get; set; }
+
+        // Primary Key - Internal
+        public int LessonId { get; set; }
+
+        // External Reference (Makandra-ID aus JSON)
+        public required string MakandraId { get; set; }
+
+        // TeachingPlan Navigation
+        public int TeachingPlanId { get; set; }
+        public TeachingPlan TeachingPlan { get; set; } = null!;
+
+        public int SortingIndex { get; set; }
 
         public required string Title { get; set; }
 
