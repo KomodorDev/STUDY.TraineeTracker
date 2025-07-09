b2d994d9f3235c56be4f3432f8df2118d068ba5e
highestPACE <pschweizer0711@gmail.com>
2025-07-09 09:38:15 +0200

    Added folder for Admin-ViewModels

--- TraineeTracker/Models/ViewModels/Admin/AdminDashboardViewModel.cs
+++ TraineeTracker/Models/ViewModels/Admin/AdminDashboardViewModel.cs
@@ -1,6 +1,6 @@
 using TraineeTracker.Models.Domain;
 
-namespace TraineeTracker.Models.ViewModels {
+namespace TraineeTracker.Models.ViewModels.Admin {
     public class AdminDashboardViewModel {
         public required IEnumerable<ApplicationUser> Users { get; set; }
         public required Dictionary<string, string> UserRoles { get; set; }
