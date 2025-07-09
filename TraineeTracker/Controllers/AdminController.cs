ff64f4701a5f038994b67e34c9885522e644eb6f
highestPACE <pschweizer0711@gmail.com>
2025-07-09 11:30:04 +0200

    Refactor ShowManageProcessingPausesView to use FindByIdWithProcessingPausesAsync and update view to directly access ProcessingPauses

--- TraineeTracker/Controllers/AdminController.cs
+++ TraineeTracker/Controllers/AdminController.cs
@@ -66,15 +66,11 @@ namespace TraineeTracker.Controllers {
 
         [HttpGet("/ProcessingPauses")]
         public async Task<IActionResult> ShowManageProcessingPausesView(string traineeId) {
-            var userTask = _applicationUserRepository.FindByIdAsync(traineeId);
-            var pausesTask = _processingPauseRepository.GetAllPausesAsync(traineeId);
-            await Task.WhenAll(userTask, pausesTask);
-            var user = await userTask;
-            if (user == null) {
+            var trainee = await _adminService.FindByIdWithProcessingPausesAsync(traineeId);
+            if (trainee == null) {
                 return NotFound();
             }
-            ViewBag.Pauses = await pausesTask;
-            return View("ManageProcessingPauses", user);
+            return View("ManageProcessingPauses", trainee);
         }
 
         [HttpGet("/CreateProcessingPause")]
