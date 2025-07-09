4b8111e2db5e8459b101f6d954fc260f32373475
schleale <alexander.schlemmer@uni-a.de>
2025-07-09 11:35:18 +0200

    Added allowed state transitions into ViewModel

--- TraineeTracker/Models/ViewModels/TraineeLessonDetailViewModel.cs
+++ TraineeTracker/Models/ViewModels/TraineeLessonDetailViewModel.cs
@@ -4,9 +4,10 @@ using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Models.ViewModels {
     public class TraineeLessonDetailViewModel {
-        public required TraineeLesson TraineeLesson {get; set;}
-        public required Lesson Lesson {get; set;}
-        public required IEnumerable<TraineeLessonLogEntry> LogEntries {get; set;}
-        public required IEnumerable<Feedback> Feedbacks {get; set;}
+        public required TraineeLesson TraineeLesson { get; set; }
+        public required Lesson Lesson { get; set; }
+        public required IEnumerable<TraineeLessonLogEntry> LogEntries { get; set; }
+        public required IEnumerable<Feedback> Feedbacks { get; set; }
+        public required IEnumerable<string> AllowedStateTransitions { get; set; }
     }
 }
