a7fb082c48527f57fb14b6f1e8d1b01a49cbcaee
highestPACE <pschweizer0711@gmail.com>
2025-07-09 11:21:54 +0200

    Refactor ApplicationUser repository methods for improved clarity and consistency

--- TraineeTracker/Data/ApplicationUsers/IApplicationUserRepository.cs
+++ TraineeTracker/Data/ApplicationUsers/IApplicationUserRepository.cs
@@ -5,45 +5,25 @@ using TraineeTracker.Models.Domain;
 namespace TraineeTracker.Data.ApplicationUsers {
     public interface IApplicationUserRepository {
         public Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role);
-
         public Task<IdentityResult> CreateAsync(ApplicationUser user, string password);
-
         public Task<bool> ExistsAsync(string userId);
-
         public Task<bool> ExistsAsync(ApplicationUser user);
-
         public Task<ApplicationUser?> FindByEmailAsync(string email);
-
         public Task<ApplicationUser?> FindByEmailWithProcessingPausesAndTraineeLessonsAsync(string email);
-
         public Task<ApplicationUser?> FindByIdAsync(string userId);
-
-        public Task<ApplicationUser?> FindByIdWithNotificationSettingAsync(string userId);
-
         public Task<ApplicationUser?> FindByIdWithLastSelectedTraineesAsync(string userId);
-
-        public Task<ApplicationUser?> FindByIdWithProcessingPausesAndTraineeLessonsAsync(string userId);
-
-        public Task<ApplicationUser?> FindByIdWithTraineeLessonsWithLessonsAndTeachingPlanAsync(string userId);
-
+        public Task<ApplicationUser?> FindByIdWithNotificationSettingAsync(string userId);
+        public Task<ApplicationUser?> FindByIdWithTeachingPlanAndTraineeLessonsWithLessonsAsync(string userId);
+        public Task<ApplicationUser?> FindByIdWithTraineeLessonsAndProcessingPausesAsync(string userId);
         public Task<ApplicationUser?> FindByIdWithWrittenFeedbacksWithLessonAsync(string userId);
-
         public Task<IEnumerable<ApplicationUser>> GetOpenUsersInRoleAsync(string roleName);
-
         public Task<IEnumerable<ApplicationUser>> GetOpenUsersInRoleWithEmailNotificationSettingAsync(string roleName);
-
         public Task<IEnumerable<string>> GetRolesAsync(ApplicationUser user);
-
         public Task<ApplicationUser?> GetUserAsync(ClaimsPrincipal principal);
-
         public Task<IEnumerable<ApplicationUser>> GetUsersInRoleAsync(string roleName);
-
         public Task<IEnumerable<ApplicationUser>> GetUsersInRoleWithProcessingPausesAndTraineeLessonsAsync(string roleName);
-
         public Task<IEnumerable<ApplicationUser>> GetAllAsync();
-
         public Task<bool> IsInRoleAsync(ApplicationUser user, string role);
-
         public Task<IdentityResult> UpdateAsync(ApplicationUser user);
     }
 }
