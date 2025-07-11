63ce1611a4ba8c675223eddc7733dc4662af88ab
highestPACE <pschweizer0711@gmail.com>
2025-07-11 15:25:14 +0200

    Add OverlapsAsync method and use it to check new processing pauses

--- TraineeTracker/Data/ProcessingPauses/DatabaseProcessingPauseRepository.cs
+++ TraineeTracker/Data/ProcessingPauses/DatabaseProcessingPauseRepository.cs
@@ -15,11 +15,23 @@ namespace TraineeTracker.Data.ProcessingPauses {
             await _context.SaveChangesAsync();
         }
 
+        [Obsolete("Use OverlapsAsync instead.")]
         public async Task<bool> ExistsAsync(ProcessingPause processingPause) {
             return await _context.ProcessingPauses.AnyAsync(p =>
                 p.TraineeId == processingPause.TraineeId &&
                 p.StartDate == processingPause.StartDate &&
                 p.EndDate == processingPause.EndDate
+            );
+        }
+
+        public async Task<bool> OverlapsAsync(ProcessingPause processingPause) {
+            return await _context.ProcessingPauses.AnyAsync(p =>
+                p.TraineeId == processingPause.TraineeId &&
+                ((p.StartDate == processingPause.StartDate && p.EndDate == processingPause.EndDate) ||  // Same
+                (processingPause.StartDate < p.StartDate && p.StartDate < processingPause.EndDate) ||   // StartDate strictly inside new pause
+                (processingPause.StartDate < p.EndDate && p.EndDate < processingPause.EndDate) ||       // EndDate strictly inside new Pause
+                (p.StartDate == processingPause.StartDate && p.EndDate > processingPause.StartDate) ||  // Enddate lurks inside new Pause
+                (p.EndDate == processingPause.EndDate && p.StartDate < processingPause.EndDate))        // Startdate lurks inside new Pause
             );
         }
 
