9066db1438b3e7abe5d26f4119d1ac05f6fa771e
highestPACE <pschweizer0711@gmail.com>
2025-07-13 23:18:27 +0200

    Enhance Admin Dashboard with role filtering and update view model to support selected role

--- TraineeTracker/Models/ViewModels/Admin/AdminDashboardViewModel.cs
+++ TraineeTracker/Models/ViewModels/Admin/AdminDashboardViewModel.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Identity;
 using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Models.ViewModels.Admin {
     public class AdminDashboardViewModel {
         public required IEnumerable<ApplicationUser> Users { get; set; }
         public required Dictionary<string, string> UserRoles { get; set; }
+        public required IEnumerable<IdentityRole> Roles { get; set; }
+
+        public string? SelectedRole { get; set; }
     }
 }
