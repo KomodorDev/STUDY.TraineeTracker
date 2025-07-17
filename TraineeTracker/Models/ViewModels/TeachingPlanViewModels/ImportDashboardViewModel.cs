4c6bcb3cd15edbf28064d9075cf6c6490f4cc2c9
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-07-17 13:53:06 +0200

    Moved Folder and changed namespace

--- TraineeTracker/Models/ViewModels/TeachingPlanViewModels/ImportDashboardViewModel.cs
+++ TraineeTracker/Models/ViewModels/TeachingPlanViewModels/ImportDashboardViewModel.cs
@@ -1,4 +1,4 @@
-namespace TraineeTracker.Models.ViewModels.TeachingPlan {
+namespace TraineeTracker.Models.ViewModels.TeachingPlanViewModels {
     public class ImportDashboardViewModel {
         public List<ExistingTeachingPlanViewModel> ExistingTeachingPlans { get; set; } = new();
     }
