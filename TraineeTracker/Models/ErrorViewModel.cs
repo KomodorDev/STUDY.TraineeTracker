463825c3c29994cd336a2c323e17336b723bfd39
KomodorDev <sah_services@outlook.com>
2025-07-18 11:49:54 +0200

    Emptied Main

--- TraineeTracker/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TraineeTracker.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
