88d54fa4fbfea4fe2ca39f1f1d0c7f2002344ae2
schleale <alexander.schlemmer@uni-a.de>
2025-07-19 14:57:42 +0200

    Mark changed feedback as unread

--- TraineeTracker/Services/TraineeLessonDetailService.cs
+++ TraineeTracker/Services/TraineeLessonDetailService.cs
@@ -31,18 +31,22 @@ namespace TraineeTracker.Services {
 
         private readonly IServiceScopeFactory _scopeFactory;
 
+        private FeedbackService _feedbackService;
+
         public TraineeLessonDetailService(ILessonRepository databaseLessonRepository,
                                             ITraineeLessonRepository databaseTraineeLessonRepository,
                                             ITraineeLessonLogEntryRepository databaseTraineeLessonLogEntryRepository,
                                             IFeedbackRepository databaseFeedbackRepository,
                                             IApplicationUserRepository databaseApplicationUserRepository,
-                                            IServiceScopeFactory scopeFactory) {
+                                            IServiceScopeFactory scopeFactory,
+                                            FeedbackService feedbackService) {
             _databaseLessonRepository = databaseLessonRepository;
             _databaseTraineeLessonLogEntryRepository = databaseTraineeLessonLogEntryRepository;
             _databaseTraineeLessonRepository = databaseTraineeLessonRepository;
             _databaseFeedbackrepository = databaseFeedbackRepository;
             _databaseApplicationUserRepository = databaseApplicationUserRepository;
             _scopeFactory = scopeFactory;
+            _feedbackService = feedbackService;
         }
 
         private async Task CheckHasAccess(ClaimsPrincipal user, int traineeLessonId) {
@@ -201,6 +205,9 @@ namespace TraineeTracker.Services {
                 existingFeedback.HoursOfEffort = feedbackDto.HoursOfEffort ?? existingFeedback.HoursOfEffort;
 
                 await _databaseFeedbackrepository.UpdateAsync(existingFeedback);
+
+                // mark changed feedback as unread
+                await _feedbackService.MarkFeedbackAsUnreadForEveryoneAsync(existingFeedback.FeedbackId);
 
                 // sends email (different thread)
                 _ = Task.Run(async () => {
