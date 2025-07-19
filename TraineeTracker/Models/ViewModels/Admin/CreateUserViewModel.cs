d26ca95cf85ce720e9a8b5efe2eb5b0fc991f83e
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-07-20 00:51:03 +0200

    Reworked ViewModels

--- TraineeTracker/Models/ViewModels/Admin/CreateUserViewModel.cs
+++ TraineeTracker/Models/ViewModels/Admin/CreateUserViewModel.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using TraineeTracker.Models.Dtos;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace TraineeTracker.Models.ViewModels.Admin {
     public class CreateUserViewModel {
+
+        [ValidateNever]
         public ApplicationUserDto User { get; set; } = new ApplicationUserDto();
         public IEnumerable<SelectListItem> Roles = Enumerable.Empty<SelectListItem>();
         public IEnumerable<SelectListItem> TeachingPlans = Enumerable.Empty<SelectListItem>();
+        public bool ConfirmSubmission { get; set; } = false;
     }
 }
