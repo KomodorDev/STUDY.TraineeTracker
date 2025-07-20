f31bdb307341cb7dd635f15988e17943e23c1b05
KomodorDev <sah_services@outlook.com>
2025-07-21 00:19:06 +0200

    TraineeLessonState Documentation

--- TraineeTracker/Services/TraineeLessonStates/States/RatedState.cs
+++ TraineeTracker/Services/TraineeLessonStates/States/RatedState.cs
@@ -2,9 +2,26 @@ using System.Security.Claims;
 using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Services.TraineeLessonStates.States {
+
+    /// <summary>
+    /// Represents the 'Rated' state of a trainee lesson. No further transitions are allowed from this state.
+    /// </summary>
+    /// <remarks>
+    /// Code Ownership: Simon Hinterreiter (hintsimo)
+    /// </remarks>
     public class RatedState : ITraineeLessonState {
-        
+
         // ----------------------------------------------
+        /// <summary>
+        /// Returns an empty list, as no transitions are allowed from the 'Rated' state.
+        /// </summary>
+        /// <param name="user">The user attempting the state transition.</param>
+        /// <returns>
+        /// An empty list of <see cref="TraineeLessonState"/> values, indicating no valid transitions.
+        /// </returns>
+        /// <remarks>
+        /// Code Ownership: Simon Hinterreiter (hintsimo)
+        /// </remarks>
         public List<TraineeLessonState> GetAllowedLessonStateTransitions(ClaimsPrincipal user) {
 
             var transitions = new List<TraineeLessonState>();
@@ -19,8 +36,20 @@ namespace TraineeTracker.Services.TraineeLessonStates.States {
             return transitions;
         }
 
-
         // ----------------------------------------------
+        /// <summary>
+        /// Attempts to transition from the 'Rated' state to another state.
+        /// This always fails, since no transitions are allowed from 'Rated'.
+        /// </summary>
+        /// <param name="targetState">The target state to transition to.</param>
+        /// <param name="user">The user attempting the transition.</param>
+        /// <returns>This method does not return normally; it always throws.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Always thrown because no transitions are allowed from the 'Rated' state.
+        /// </exception>
+        /// <remarks>
+        /// Code Ownership: Simon Hinterreiter (hintsimo)
+        /// </remarks>
         public TraineeLessonState TransitionTo(TraineeLessonState targetState, ClaimsPrincipal user) {
             var allowed = GetAllowedLessonStateTransitions(user);
 
@@ -29,5 +58,7 @@ namespace TraineeTracker.Services.TraineeLessonStates.States {
             }
             return targetState;
         }
+
+        // ----------------------------------------------
     }
 }
