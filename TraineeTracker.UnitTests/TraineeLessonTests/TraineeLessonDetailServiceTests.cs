3c8bfdfb3af54fe0426bc610a87e138269981cd9
schleale <alexander.schlemmer@uni-a.de>
2025-07-20 22:26:59 +0200

    Created traineelessondetailservicetests class

---
+++ TraineeTracker.UnitTests/TraineeLessonTests/TraineeLessonDetailServiceTests.cs
@@ -0,0 +1,57 @@
+using TraineeTracker.Models.Domain;
+using TraineeTracker.Services;
+using TraineeTracker.Models.Dtos;
+
+namespace TraineeTracker.UnitTests.TraineeLessonTests {
+
+        /// <summary>
+        /// A class for testing functionality of the TraineeLessonDetailService.cs
+        /// </summary>
+        /// <remarks>
+        /// Code Ownership: Alexander Schlemmer (schleale)
+        /// </remarks>
+        public class TraineeLessonDetailServiceTests {
+                // ----------------------------------------------------
+                [Fact]
+                public async Task SaveTraineeLessonStateChangeTest_FinishedToAccepted_ByMentor() {
+                        var service = new TraineeLessonDetailService();
+
+                        // ---
+                        var trainee = TestDataFactoryTraineeLessons.CreateTestApplicationUser(1);
+                        var mentor = TestDataFactoryTraineeLessons.CreateTestApplicationUser(2);
+
+                        var lesson = TestDataFactoryTraineeLessons.CreateTestLesson(1);
+                        var traineeLesson = TestDataFactoryTraineeLessons.CreateTestTraineeLesson(trainee, lesson);
+
+                        // ---
+                        await service.SaveTraineeLessonStateChange(new TraineeLessonDto {
+                                TraineeLessonId = traineeLesson.TraineeLessonId,
+                                TraineeId = trainee.Id,
+                                LessonId = lesson.LessonId,
+                                TargetStateName = TraineeLessonState.Accepted.ToString(),
+                                RejectionReason = null
+                        },);
+                }
+
+                // ----------------------------------------------------
+                [Fact]
+                public async Task SaveFeedbackTest_FeedbackDoesNotExist() {
+                        var service = new TraineeLessonDetailService();
+
+                        // ---
+                        var trainee = TestDataFactoryTraineeLessons.CreateTestApplicationUser(1);
+
+                        var lesson = TestDataFactoryTraineeLessons.CreateTestLesson(1);
+                        var traineeLesson = TestDataFactoryTraineeLessons.CreateTestTraineeLesson(trainee, lesson);
+
+                        var feedbackDto = new FeedbackDto {
+                                Difficulty = LessonDifficulty.VeryEasy,
+                                PreviousKnowledge = PreviousKnowledgeLevel.None,
+                                HoursOfEffort = (float) 1.1,
+                                TraineeLessonId = 
+                        };
+
+                        await service.SaveFeedback(feedbackDto, );
+                }
+        }
+}
