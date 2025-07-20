3d4adfa54cf45e87974cccf82ba4c5bd3e1cdb33
Simon Hinterreiter <simon.hinterreiter@uni-a.de>
2025-07-20 17:24:28 +0200

    Hotfix: In-Memory DB working again

--- TraineeTracker/Program.cs
+++ TraineeTracker/Program.cs
@@ -96,10 +96,15 @@ builder.Services.AddScoped<TestDataSeeder>();
 
 var app = builder.Build();
 
+// ----------------------------------------
 // Ensure DB is there:
 using (var scope = app.Services.CreateScope()) {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
+    var database = db.Database;
+
+    if (database.IsRelational()) {
+        database.Migrate();
+    }
 }
 
 // ---------------------------------------------
