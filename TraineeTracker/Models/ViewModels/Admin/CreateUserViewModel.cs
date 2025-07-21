a1a175402066822fd68314e79d4ebeedf6874b04
highestPACE <pschweizer0711@gmail.com>
2025-07-21 13:37:44 +0200

    Remove ValidateNever attribute from User property in CreateUserViewModel

--- TraineeTracker/Models/ViewModels/Admin/CreateUserViewModel.cs
+++ TraineeTracker/Models/ViewModels/Admin/CreateUserViewModel.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace TraineeTracker.Models.ViewModels.Admin {
     public class CreateUserViewModel {
-
-        [ValidateNever]
         public ApplicationUserDto User { get; set; } = new ApplicationUserDto();
         public IEnumerable<SelectListItem> Roles = Enumerable.Empty<SelectListItem>();
         public IEnumerable<SelectListItem> TeachingPlans = Enumerable.Empty<SelectListItem>();
