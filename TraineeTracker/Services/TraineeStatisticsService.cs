ae2bdb98da006edbe554148a870dff8d38c24bd7
stefanni <nikita.stefan@uni-a.de>
2025-07-23 15:38:53 +0200

    nicht verwendete usings im service entfernt

--- TraineeTracker/Services/TraineeStatisticsService.cs
+++ TraineeTracker/Services/TraineeStatisticsService.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Security.Claims;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 using TraineeTracker.Data.TraineeStatistics;
 using TraineeTracker.Models.ViewModels;
 using TraineeTracker.Models.Domain;
