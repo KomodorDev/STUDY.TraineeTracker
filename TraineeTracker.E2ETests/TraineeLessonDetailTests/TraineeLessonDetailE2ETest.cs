a46530a69ea74e4ddd2fe1d26d8fa7ef01e6c8cb
schleale <alexander.schlemmer@uni-a.de>
2025-07-23 15:52:25 +0200

    Created e2e test class

---
+++ TraineeTracker.E2ETests/TraineeLessonDetailTests/TraineeLessonDetailE2ETest.cs
@@ -0,0 +1,12 @@
+namespace TraineeTracker.E2ETests.TraineeLessonDetailTests {
+    
+    /// <summary>
+    /// A class for End-2-End tests
+    /// </summary>
+    /// <remarks>
+    /// Code Ownership: Alexander Schlemmer (schleale)
+    /// </remarks>
+    public class TraineeLessonDetailE2ETest {
+
+    }
+}
