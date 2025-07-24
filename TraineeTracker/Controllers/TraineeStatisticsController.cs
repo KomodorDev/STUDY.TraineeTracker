4871929cdaf6144f94e63bf949b2d42ed32c8239
stefanni <nikita.stefan@uni-a.de>
2025-07-24 12:40:48 +0200

    controller aufgeräumt

--- TraineeTracker/Controllers/TraineeStatisticsController.cs
+++ TraineeTracker/Controllers/TraineeStatisticsController.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using TraineeTracker.Services;
-using TraineeTracker.Models.ViewModels;
 
 namespace TraineeTracker.Controllers {
     public class TraineeStatisticsController : Controller {
@@ -10,20 +8,13 @@ namespace TraineeTracker.Controllers {
         public TraineeStatisticsController(TraineeStatisticsService service) {
             _service = service;
         }
-/*         [Route("TraineeStatistics/{traineeId}")]
-        [HttpGet] */
+
         [HttpGet("TraineeStatistics/{traineeId}")]
         public async Task<IActionResult> ShowTraineeStatisticsDashboardView(string traineeId) {
-            Console.WriteLine("View aufrufen");
+            
             var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            Console.WriteLine($"[TraineeStatisticsController.ShowTraineeStatisticsDashboardView] traineeId = {traineeId}");
-
-            Console.WriteLine($"[TraineeStatisticsController.ShowTraineeStatisticsDashboardView] currentUserId = {currentUserId}");
-
-
             if (currentUserId == null) {
-                Console.WriteLine("UserId = null");
                 return Unauthorized();
             }
 
@@ -32,10 +23,8 @@ namespace TraineeTracker.Controllers {
             var isTraineeSelf = currentUserId == traineeId;
 
             if (!(isAdmin || isMentor || isTraineeSelf)) {
-                Console.WriteLine("falscher User");
                 return Forbid();
             }
-            Console.WriteLine("TraineeStatisticsController: ViewModel bauen und View final anzeigen");
 
             var model = await _service.BuildTraineeStatisticsViewModel(traineeId, User);
             return PartialView("_TraineeStatistics", model);
