3526120d1a02378276f698cd24baa400163085a9
highestPACE <pschweizer0711@gmail.com>
2025-07-30 17:19:36 +0200

    Add Display attributes for clarity in ApplicationUserDto properties

--- TraineeTracker/Models/Dtos/ApplicationUserDto.cs
+++ TraineeTracker/Models/Dtos/ApplicationUserDto.cs
@@ -13,6 +13,7 @@ namespace TraineeTracker.Models.Dtos {
         /// </summary>
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; } = "";
 
         /// <summary>
@@ -23,6 +24,7 @@ namespace TraineeTracker.Models.Dtos {
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; } = "Sopro.2025";
 
         /// <summary>
@@ -30,12 +32,14 @@ namespace TraineeTracker.Models.Dtos {
         /// Required field.
         /// </summary>
         [Required]
+        [Display(Name = "Role")]
         public string Role { get; set; } = "";
 
         /// <summary>
         /// Gets or sets the ID of the associated teaching plan.
         /// Nullable; may not be set for all users.
         /// </summary>
+        [Display(Name = "Teachingplan")]
         public int? TeachingPlanId { get; set; }
 
         /// <summary>
@@ -43,6 +47,7 @@ namespace TraineeTracker.Models.Dtos {
         /// Only applicable for users with the Trainee role.
         /// Nullable; defaults to today's date.
         /// </summary>
+        [Display(Name = "Start Date")]
         public DateOnly? TraineeStartDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
 
         /// <summary>
@@ -50,6 +55,7 @@ namespace TraineeTracker.Models.Dtos {
         /// Only applicable for users with the Trainee role.
         /// Nullable; defaults to today's date.
         /// </summary>
+        [Display(Name = "End Date")]
         public DateOnly? TraineeEndDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
     }
 }
