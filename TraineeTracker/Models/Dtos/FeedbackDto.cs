e0eb1fdf7df0ec423bd67800f8b66820ffb2cb50
blaskale <alexandros.blask@uni-a.de>
2025-08-01 17:04:30 +0200

    Summary für die Dtos

--- TraineeTracker/Models/Dtos/FeedbackDto.cs
+++ TraineeTracker/Models/Dtos/FeedbackDto.cs
@@ -2,27 +2,48 @@ using System.ComponentModel.DataAnnotations;
 using TraineeTracker.Models.Domain;
 
 namespace TraineeTracker.Models.Dtos {
+
+    // ------------------------------------------------------
+    /// <summary>
+    /// Provides Dto for the Feedback
+    /// Code Ownership: Alexandros Blask
+    /// </summary>
     public class FeedbackDto {
         
-        public required LessonDifficulty Difficulty {
-            get; set;
-        }
+        // ------------------------------------------------------
+        /// <summary>
+        /// Difficulty of the Lesson
+        /// Code Ownership: Alexandros Blask
+        /// </summary>
+        public required LessonDifficulty Difficulty { get; set; }
 
-        public required PreviousKnowledgeLevel PreviousKnowledge {
-            get; set;
-        }
+        // ------------------------------------------------------
+        /// <summary>
+        /// Previous Knowledge of the Trainee
+        /// Code Ownership: Alexandros Blask
+        /// </summary>
+        public required PreviousKnowledgeLevel PreviousKnowledge { get; set; }
 
-        public required float HoursOfEffort {
-            get; set;
-        }
+        // ------------------------------------------------------
+        /// <summary>
+        /// The hours of effort the Trainee had with the lesson
+        /// Code Ownership: Alexandros Blask
+        /// </summary>
+        public required float HoursOfEffort { get; set; }
 
-        public string? Comment {
-            get; set;
-        }
+        // ------------------------------------------------------
+        /// <summary>
+        /// Comment of the Feedback
+        /// Code Ownership: Alexandros Blask
+        /// </summary>
+        public string? Comment { get; set; }
 
-        public required int TraineeLessonId {
-            get; set;
-        }
+        // ------------------------------------------------------
+        /// <summary>
+        /// The TraineeLessonId for the Feedback
+        /// Code Ownership: Alexandros Blask
+        /// </summary>
+        public required int TraineeLessonId { get; set; }
     }
 }
 
