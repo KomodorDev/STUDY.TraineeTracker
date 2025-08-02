a24e239f6c575f6da458b03693de9e31754894d6
KomodorDev <sah_services@outlook.com>
2025-08-02 13:39:00 +0200

    EnumExtension Doku

--- TraineeTracker/Extensions/EnumExtensions.cs
+++ TraineeTracker/Extensions/EnumExtensions.cs
@@ -1,8 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
-namespace TraineeTracker.Extensions{
+namespace TraineeTracker.Extensions {
+
+    /// <summary>
+    /// Provides extension methods for working with enumeration types.
+    /// </summary>
+    /// <remarks>Code Ownership: Simon Hinterreiter (hintsimo)</remarks>
     public static class EnumExtensions {
+
+        // ------------------------------------------------------
+        /// <summary>
+        /// Retrieves the value of the <see cref="DisplayAttribute.Name"/> property 
+        /// for a given enum value. If no <see cref="DisplayAttribute"/> is applied,
+        /// the enum's name is returned as a fallback.
+        /// </summary>
+        /// <param name="enumValue">The enum value whose display name should be retrieved.</param>
+        /// <returns>
+        /// The string specified by the <see cref="DisplayAttribute.Name"/>; 
+        /// otherwise, the enum value’s name as a string.
+        /// </returns>
+        /// <remarks>Code Ownership: Simon Hinterreiter (hintsimo)</remarks>
         public static string GetDisplayName(this Enum enumValue) { // this: Signals that this is an extension for enums
             return enumValue
                 .GetType()
@@ -12,5 +30,7 @@ namespace TraineeTracker.Extensions{
                 .GetName()
                 ?? enumValue.ToString();
         }
+
+        // ------------------------------------------------------
     }
 }
