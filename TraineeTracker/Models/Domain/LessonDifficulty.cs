822754386f6c16d342e20c1326752829b127c7cd
schleale <alexander.schlemmer@uni-a.de>
2025-08-02 14:08:15 +0200

    Enum Documentation

--- TraineeTracker/Models/Domain/LessonDifficulty.cs
+++ TraineeTracker/Models/Domain/LessonDifficulty.cs
@@ -1,6 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace TraineeTracker.Models.Domain {
+
+    /// <summary>
+    /// Represents the possible difficulties, that a user can select during feedback creation/editing in the corresponding dropdown menu.
+    /// </summary>
+    /// <remarks>
+    /// Code Ownership: Simon Hinterreiter (hintsimo)
+    /// </remarks>
     public enum LessonDifficulty {
         [Display(Name = "Very Easy")]
         VeryEasy,
