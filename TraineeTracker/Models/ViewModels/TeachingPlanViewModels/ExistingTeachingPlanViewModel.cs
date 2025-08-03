cd518fb8fe87d316300118a8bbdf960628f73965
blaskale <alexandros.blask@uni-a.de>
2025-08-03 13:24:32 +0200

    Short Summaries for the attributes of Preview Dtos

--- TraineeTracker/Models/ViewModels/TeachingPlanViewModels/ExistingTeachingPlanViewModel.cs
+++ TraineeTracker/Models/ViewModels/TeachingPlanViewModels/ExistingTeachingPlanViewModel.cs
@@ -9,14 +9,29 @@ namespace TraineeTracker.Models.ViewModels.TeachingPlanViewModels {
     /// </remarks>
     public class ExistingTeachingPlanViewModel {
         
+        /// <summary>
+        /// The Unique Identifier for the ViewModel
+        /// </summary>
         public required int TeachingPlanId { get; set; }
         
+        /// <summary>
+        /// Name of the TeachingPlan
+        /// </summary>
         public required string Name { get; set; }
         
+        /// <summary>
+        /// The time the teaching plan was last updated
+        /// </summary>
         public DateTime LastUpdated { get; set; }
         
+        /// <summary>
+        /// The amount of lessons of the TeachingPlan
+        /// </summary>
         public int LessonCount { get; set; }
         
+        /// <summary>
+        /// The amount of trainees assigned to the TeachingPlan
+        /// </summary>
         public int TraineeCount { get; set; }
     }
 }
